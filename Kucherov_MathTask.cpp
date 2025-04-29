2bfcdcb95b42ea0f07fac31d71fef5daf84f9244
VoV14-88 <kucerovvova36@gmail.com>
2025-04-29 10:13:55 +0300

    add Kucherov_MathTask.h

---
+++ Kucherov_MathTask.cpp
@@ -0,0 +1,20 @@
+// Simname_MathTask.cpp > main()
+#include <iostream>
+#include "Kucherov_MathTask.h"
+
+using namespace std;
+
+int main() {
+    int NumberA = 0, NumberB = 0;
+    // Ввод переменной A
+    EnterDigit(NumberA, "input height A");
+    // Ввод переменной B
+    EnterDigit(NumberB, "input height B");
+
+    // Вычисление площади прямоугольника
+    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+    // Вывод значения площади
+    cout << "Area of Rectangle is " << RectangleArea << endl;
+
+    return 0;
+}
